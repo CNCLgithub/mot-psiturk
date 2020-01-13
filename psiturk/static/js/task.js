1da2d8f1a063985c6a2ef44e0bd3a16a4105faaa
belledon <mbelledonne@gmail.com>
2020-01-13 11:45:47 -0500

    added image scaling

--- psiturk/static/js/task.js
+++ psiturk/static/js/task.js
@@ -1,6 +1,5 @@
 /*
  * Requires:
- *     plane-draggable.js
  *     psiturk.js
  *     utils.js
  */
@@ -8,32 +7,15 @@
 // Initalize psiturk object
 var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 
-// Objects to keep track of the current phase and state
-var CURRENTVIEW;
-var STATE;
-
+// Names of elements used in the experiment
 var MOVIESCREEN = "moviescreen";
 var DRAGBOX = "dragbox"
 var NEXTBUTTON = "nextbutton";
 var PROGRESS = "progress";
 var RELOAD = "reloadbutton";
-
-
 var INS_INSTRUCTS = "instruct";
 var INS_HEADER = "instr_header";
-
-var COLOR_MAP = {
-  'R': "red",
-  'G': "green",
-  'B': "blue"
-};
-
-function map_colors(colors) {
-  var f = function(c) {
-    return COLOR_MAP[c]
-  };
-  return colors.map(f)
-};
+var PAGESIZE = 500;
 
 var IMG_TIME = 100 // time to display images in ms
 
@@ -49,16 +31,10 @@ var pages = [
 
 psiTurk.preloadPages(pages);
 
-
 var instructionPages = [ // add as a list as many pages as you like
   "instructions/instruct-1.html"
 ];
 
-
-// returns true if we are showing the dragger
-function usingResponse(str) {
-  return (str === 'show' | str === "use");
-};
 
 // used to shuffle the array of trials
 function shuffle(array) {
@@ -102,7 +78,9 @@ var make_img = function(imgname, is_intro, freeze) {
   if (freeze) {
     r += "onload=\"cut2black()\" "
   }
-  r += "class=\"" + mcl + "\" src=\"static/data/" + imgname + "\" alt=\"Movie\">"
+  // r += "class=\"" + mcl + "\" src=\"static/data/" + imgname +
+  //   "\" alt=\"Movie\" width=>"
+  r += `class="${mcl}" src="static/data/${imgname}" alt="Movie" style="height: auto; width: ${PAGESIZE}">`
   return r
 };
 
@@ -129,32 +107,26 @@ var make_mov = function(movname, is_intro, has_ctr) {
  *
  ********************/
 
-
-function makeButton(color, size) {
-  var s = `<div id=\"${color}\" class=\"circle\" "></div>`;
-  return s;
-};
-
-function makeLine() {
-  return "<hr /><div id=\"finish\" class=\"finish-line\">" +
-    "<div class=\"topLeft\">Very light</div>" +
-    "<div class=\"topRight\">Very heavy</div>" +
-    "</div>";
+function allowNext() {
+  console.log("here");
+  document.getElementById(NEXTBUTTON).disabled = false;
+  console.log(document.getElementById(NEXTBUTTON).disable);
 }
 
+function makeCheckBox() {
 
-// formats and injects html for slider
-function makeSlider(labels) {
-  nLabels = labels.length;
-  dWidth = 80.0 / nLabels;
-  dHeight = 80.0 / nLabels;
-  var s = makeLine();
-  for (var ci in labels) {
-    var color = labels[ci];
-    s += makeButton(color, dWidth);
-  }
-  return s
+  return "<form action=\"\" method=\"post\" id=\"trial_response\">" +
+    "<input type=\"radio\" name=\"yes_box\" value=true onClick=\"allowNext()\"/> Yes" +
+    "<input type=\"radio\" name=\"nay_box\" value=false onClick=\"allowNext()\"/> No" +
+    "</form>"
+
 };
+
+function makeSlider() {
+  return "<span id=\"qspan\">Move the slider to match your card</span>"+
+    "<input id=\"scale_slider\" type=\"range\" min=\"0\" max=\"100\" default=\"50\" width=\"1500\"/>";
+}
+
 
 class Page {
 
@@ -163,22 +135,17 @@ class Page {
   /*******************
    * Public Methods  *
    *******************/
-  constructor(text, mediatype, mediapath, colors, mask = false) {
+  constructor(text, mediatype, mediapath, show_response = false) {
     // page specific variables
     this.text = text;
     this.mediatype = mediatype;
     this.mediapath = mediapath;
-    console.log(mediapath);
-    this.mask = mask;
-    // slider relevant variables
-    this.colors = colors;
-    this.showSlider = (colors.length > 0);
-    this.inPlace = Array(colors.length).fill(false);
-    this.tolerance = 8.0;
-    this.offest = 3.0;
+    this.mask = false;
+    this.pageSize = PAGESIZE;
     // html elements
     this.instruct = document.getElementById(INS_INSTRUCTS);
-    this.dragbox = document.getElementById(DRAGBOX);
+    this.response = document.getElementById("response_region");
+    this.showResponse = show_response;
     this.next = document.getElementById(NEXTBUTTON);
     this.next.disable = true;
     this.mvsc = document.getElementById(MOVIESCREEN);
@@ -200,16 +167,9 @@ class Page {
   }
 
   // Returns the placement of each color scaled from [0, 1]
-  sliderValue() {
-    var bb = this.dragbox.getBoundingClientRect();
-    var responses = {};
-    for (var ci in this.colors) {
-      var color = this.colors[ci];
-      var cElem = document.getElementById(color);
-      var rect = cElem.getBoundingClientRect();
-      responses[color] = (rect.left + rect.width / 2.0 - bb.left) / bb.width;
-    }
-    return responses
+  retrieveResponse() {
+    var form = document.getElementById("trial_response");
+    return form.value
   }
 
 
@@ -232,82 +192,45 @@ class Page {
     } else if (this.mediatype === 'movie') {
       this.mvsc.innerHTML = make_mov(this.mediapath, true);
       this.showMovie();
+    } else if (this.mediatype == 'scale'){
+      this.mvsc.innerHTML = make_img(this.mediapath, true, false) + "<br>";
+      this.scalePage();
+
     } else {
       this.mvsc.innerHTML = "";
       this.showImage();
     }
   };
 
-  // Enables slider
-  enableSlider() {
-    // enable draggables
-    for (var c in this.colors) {
-      var color = this.colors[c];
-      var draggable = document.getElementById(color);
-      draggable.disabled = false;
+  scalePage() {
+    this.response.innerHTML = makeSlider();
+    var slider_value = document.getElementById("scale_slider");
+    var scale_img = document.getElementById("thisimg");
+    slider_value.oninput = function(e) {
+      PAGESIZE = (e.target.value / 50.0) * 500;
+      console.log(PAGESIZE)
+      scale_img.style.width = `${PAGESIZE}px`;
     }
+
   }
 
-  // disables slider
-  disableSlider() {
-    for (var ci in this.colors) {
-      var color = this.colors[ci];
-      var draggable = document.getElementById(color);
-      draggable.disabled = true;
-    }
+  addResponse() {
+    this.response.innerHTML = makeCheckBox();
   }
 
-
-  // creates and adds hooks for an individual draggable
-  makeDraggable(elemName) {
-    var offset = 3,
-      tolerance = 8,
-      dragElem = document.getElementById(elemName),
-      cIdx = this.colors.indexOf(elemName),
-      target = document.getElementById('finish');
-    var targets = [{
-      // snap to the scale rect
-      boundingBox: target,
-      // using gravity for qol
-      gravity: 50,
-      // snap to the inside of the target
-      edge: 'inside'
-    }];
-    var me = this;
-    new PlainDraggable(dragElem, {
-      disabled: true,
-      snap: {
-        targets: targets,
-        // base: 'document'
-        base: 'containment'
-      },
-      onDragEnd: function(newPosition) {
-        // Get drop-box that the element was dropped.
-        var tarRect = target.getBoundingClientRect();
-        var myRect = dragElem.getBoundingClientRect();
-        me.inPlace[cIdx] = Math.abs(myRect.y - tarRect.top) < tolerance;
-        console.log(myRect.y, newPosition.top, tarRect.top);
-        mClassList(target).toggle('finished',
-          (me.inPlace.every(Boolean)));
-        if (me.inPlace.every(Boolean)) {
-          me.next.disabled = false;
-        } else {
-          me.next.disabled = true;
-        }
-      },
-    });
+  // The form will automatically enable the next button
+  enableResponse() {
+    var form = document.getElementById("trial_response");
+    form.disabled = false;
   }
 
-
-  // formats html for slider
-  addSlider() {
-    // The slider is initially disabled until after the media is played
-    this.dragbox.innerHTML = makeSlider(this.colors);
-    for (var c in this.colors) {
-      this.makeDraggable(this.colors[c]);
-    }
+  disableResponse() {
+    document.getElementById("trial_response").disabled = true;
   }
 
+  clearResponse() {
+    this.response.innerHTML = "";
+  }
 
   // plays movie
   showMovie() {
@@ -318,13 +241,12 @@ class Page {
     let me = this;
 
     // The "next" botton will only activate after recording a response
-    if (this.showSlider) {
+    if (this.showResponse) {
       var movOnEnd = function() {
         if (me.mask) {
           cut2black();
         }
-        me.addSlider();
-        me.enableSlider();
+        me.enableResponse();
       };
     } else {
       // Otherwise allow next once movie is complete
@@ -342,18 +264,13 @@ class Page {
   }
 
   showImage() {
-    if (this.showSlider) {
+    if (this.showResponse) {
       this.next.disabled = true;
-      this.addSlider();
-      this.enableSlider();
+      this.addResponse();
+      this.enableResponse();
     } else {
       this.next.disabled = false;
     }
-  }
-
-
-  clearSlider() {
-    this.dragbox.innerHTML = "";
   }
 };
 
@@ -371,71 +288,73 @@ var InstructionRunner = function(condlist) {
   var nTrials = condlist.length;
 
 
-  // Series of Instructions: [text, stim type, stim path, use slider]
   var instructions = [
-    ["In this task, you will observe <b>three</b> colored balls interact on a ramp.<br>" +
-      "The color of each ball indicates its weight. " +
-      "In other words, two balls with the <b>same</b> color have the same mass.<br>" +
-      "Your job is to compare the weight of each ball in association with its color.",
-      "none", "none", []
+    ["Instructions go here.",
+      "scale", "test.png", false
     ],
+    // ["In this task, you will observe <b>three</b> colored balls interact on a ramp.<br>" +
+    //   "The color of each ball indicates its weight. " +
+    //   "In other words, two balls with the <b>same</b> color have the same mass.<br>" +
+    //   "Your job is to compare the weight of each ball in association with its color.",
+    //   "none", "none", []
+    // ],
 
-    ["A ball can be colored either <span style=\"color:red;\">red</span>, <span style=\"color:blue;\">blue</span>, or <span style=\"color:green;\">green</span>.<br>" +
-      "However, the relationship between a particular color and weight can change between scenes.<br>" +
-      "<br>For example, the <span style=\"color:red;\">red</span> ball on the left is heavier than <span style=\"color:blue;\">blue</span>.<br>" +
-      "<hr /><i>Note</i>: After the video ends, please press the 'Next' button at the bottom of the screen to move on.",
-      "movie", "intro_1_t-900.mp4", []
-    ],
+    // ["A ball can be colored either <span style=\"color:red;\">red</span>, <span style=\"color:blue;\">blue</span>, or <span style=\"color:green;\">green</span>.<br>" +
+    //   "However, the relationship between a particular color and weight can change between scenes.<br>" +
+    //   "<br>For example, the <span style=\"color:red;\">red</span> ball on the left is heavier than <span style=\"color:blue;\">blue</span>.<br>" +
+    //   "<hr /><i>Note</i>: After the video ends, please press the 'Next' button at the bottom of the screen to move on.",
+    //   "movie", "intro_1_t-900.mp4", []
+    // ],
 
-    ["In this case, the <span style=\"color:blue;\">blue</span> ball is heavier than the <span style=\"color:red;\">red</span> one.",
-      "movie", "intro_2_t-700.mp4", []
-    ],
+    // ["In this case, the <span style=\"color:blue;\">blue</span> ball is heavier than the <span style=\"color:red;\">red</span> one.",
+    //   "movie", "intro_2_t-700.mp4", []
+    // ],
 
-    ["To register your decision you will see a scale, like the one below, that will ask you to order the weight of each color<br> " +
-      "<b>You must drag all the colors into the scale in order to progress.</b><br>" +
-      "Please try this out before continuing.",
-      "none", "none", ["red", "blue", "green"]
-    ],
+    // ["To register your decision you will see a scale, like the one below, that will ask you to order the weight of each color<br> " +
+    //   "<b>You must drag all the colors into the scale in order to progress.</b><br>" +
+    //   "Please try this out before continuing.",
+    //   "none", "none", ["red", "blue", "green"]
+    // ],
 
-    ["<b>Not</b> all trials will have all three colors.<br>" +
-      "The slider will reflect the number of unique colors present in the movie.",
-      "none", "none", ["blue", "green"]
-    ],
+    // ["<b>Not</b> all trials will have all three colors.<br>" +
+    //   "The slider will reflect the number of unique colors present in the movie.",
+    //   "none", "none", ["blue", "green"]
+    // ],
 
-    ["As soon as the movie ends, the scale will appear beneath it.<br>" +
-      "You can then drag the balls onto the scale to make your judgments of their weight.<br>" +
-      "After you are done, press `Next' to move on.",
-      "movie", "intro_1_t-335.mp4", ["red", "blue", "green"]
-    ],
+    // ["As soon as the movie ends, the scale will appear beneath it.<br>" +
+    //   "You can then drag the balls onto the scale to make your judgments of their weight.<br>" +
+    //   "After you are done, press `Next' to move on.",
+    //   "movie", "intro_1_t-335.mp4", ["red", "blue", "green"]
+    // ],
 
-    ["Finally, the lights will shut off during or after objects are in motion.<br>" +
-      "Here, the light go dark after the first collision.<br>" +
-      "<hr /><i>Note</i>: Do <b>NOT</b> refresh your page. In each trial, you will only observe the video once",
-      "movie", "intro_1_t-335.mp4", ["red", "blue", "green"], true
-    ],
+    // ["Finally, the lights will shut off during or after objects are in motion.<br>" +
+    //   "Here, the light go dark after the first collision.<br>" +
+    //   "<hr /><i>Note</i>: Do <b>NOT</b> refresh your page. In each trial, you will only observe the video once",
+    //   "movie", "intro_1_t-335.mp4", ["red", "blue", "green"], true
+    // ],
 
-    ["To help you keep track of balls associated with each color, the left most ball will always" +
-      " be <span style=\"color:red;\">red</span>, followed by <span style=\"color:blue;\">blue</span>" +
-      " and finally, <span style=\"color:green;\">green</span><br>" +
-      "In addition, the scale will also match this order except from top to bottom" +
-      "<hr /><i>Note</i>: In the experiment, the video will cut to black, not freeze.",
-      "movie", "intro_1_t-335.mp4", ["red", "blue", "green"]
-    ],
+    // ["To help you keep track of balls associated with each color, the left most ball will always" +
+    //   " be <span style=\"color:red;\">red</span>, followed by <span style=\"color:blue;\">blue</span>" +
+    //   " and finally, <span style=\"color:green;\">green</span><br>" +
+    //   "In addition, the scale will also match this order except from top to bottom" +
+    //   "<hr /><i>Note</i>: In the experiment, the video will cut to black, not freeze.",
+    //   "movie", "intro_1_t-335.mp4", ["red", "blue", "green"]
+    // ],
 
-    ["Here is the same movie but with the lights shutting off.",
-      "movie", "intro_1_t-335.mp4", ["red", "blue", "green"], true
-    ],
+    // ["Here is the same movie but with the lights shutting off.",
+    //   "movie", "intro_1_t-335.mp4", ["red", "blue", "green"], true
+    // ],
 
-    ["If there are several balls with the same color it is possible to have a  <span style=\"color:red;\">red</span>" +
-      " ball after a <span style=\"color:blue;\">blue</span> one, however, the slider will still preserve the order" +
-      " showing the <span style=\"color:red;\">red</span> at the top",
-      "movie", "intro_2_t-700.mp4", ["red", "blue"], true
-    ],
+    // ["If there are several balls with the same color it is possible to have a  <span style=\"color:red;\">red</span>" +
+    //   " ball after a <span style=\"color:blue;\">blue</span> one, however, the slider will still preserve the order" +
+    //   " showing the <span style=\"color:red;\">red</span> at the top",
+    //   "movie", "intro_2_t-700.mp4", ["red", "blue"], true
+    // ],
 
-    ["After a short check to make sure that you have understood the instructions, " +
-      "you will have to make your judgments about " + nTrials + " trials.<br>",
-      "none", "none", []
-    ],
+    // ["After a short check to make sure that you have understood the instructions, " +
+    //   "you will have to make your judgments about " + nTrials + " trials.<br>",
+    //   "none", "none", []
+    // ],
   ];
   var ninstruct = instructions.length;
 
@@ -446,7 +365,7 @@ var InstructionRunner = function(condlist) {
     if (i < ninstruct) {
       var page = new Page(...instructions[i]);
       page.showPage(function() {
-        page.clearSlider();
+        page.clearResponse();
         do_page(i + 1);
       });
     } else {
@@ -559,7 +478,7 @@ var Experiment = function(triallist) {
   // Record the subject's response for a given trial.
   var register_response = function(trialPage, cIdx) {
     var rt = new Date().getTime() - starttime;
-    var ch = trialPage.sliderValue();
+    var ch = trialPage.responseValue();
     // Records as [trialname, choice of mass, reaction time]
     psiTurk.recordTrialData({
       'TrialName': triallist[cIdx][0],
@@ -569,7 +488,7 @@ var Experiment = function(triallist) {
       'TrialOrder': cIdx
     });
     // Clears slider from screen
-    trialPage.clearSlider();
+    trialPage.clearReponse();
   };
 
   var end = function() {
@@ -669,7 +588,7 @@ $(window).load(function() {
       url: "static/data/condlist.json",
       async: false,
       success: function(data) {
-        condlist = shuffle(data[condition]);
+        condlist = shuffle(data[0]);
         InstructionRunner(condlist);
       },
       error: function() {
