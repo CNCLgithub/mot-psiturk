c0b3cbe5f936732fb6267fbe34add88576fe1131
belledon <mbelledonne@gmail.com>
2020-01-20 14:14:30 -0500

    removed comments

--- psiturk/static/js/task.js
+++ psiturk/static/js/task.js
@@ -321,39 +321,36 @@ var InstructionRunner = function(condlist) {
   // 4: Whether to show the response div (true/false)
 
   var instructions = [
-    // [
-    //   "In this task, you will observe a series of dots move on the screen.<br>",
-    //   "image", "test.png", false
-    // ],
-    // // image with target labels (red)
-    // [
-    //   "At the beginning of each trial, you will see <b>4</b> of the <b>8</b> dots highlighted <span style=\"color:red;\">red</span> "+
-    //     "designating them as <b>targets</b>.<br>" +
-    //     "Shortly after, the <span style=\"color:red;\">red</span> labels will dissapear and the dots will begin to move.<br>" +
-    //   "image", "test.png", false
-    // ],
-    // [
-    //   "Your task is to keep track of the <b>targets</b> as they move throughout the scene.<br>",
-    //   "movie", "test.mp4", false
-    // ],
-    // [
-    //   "At the end of each trial, <b>1</b> of the <b>8</b> dots will be highlighted in <span style=\"color:blue;\">blue</span>" +
-    //     ".<br> Your job is to judge whether that dot was one of the <b>targets</b>.",
-    //   "movie", "test.mp4", false
-    // ],
-    // [
-    //   "You will be able to record your response by clicking on one of the two check boxes shown below. <br>",
-    //     "<hr /><i>Note</i>: You will <b>NOT</b> be able to progress to the next trial until you have submitted a response.",
-    //   "text", "", true
-    // ],
-    // [
-    //   "You will be able to record your response by clicking on one of the two check boxes shown below. <br>" +
-    //     "<hr /><i>Note</i>: You will <b>NOT</b> be able to record your response until the video has <b>completed</b>",
-    //   "movie", "test.mp4", true
-    // ],
-
-
-
+    [
+      "In this task, you will observe a series of dots move on the screen.<br>",
+      "image", "test.png", false
+    ],
+    // image with target labels (red)
+    [
+      "At the beginning of each trial, you will see <b>4</b> of the <b>8</b> dots highlighted <span style=\"color:red;\">red</span> "+
+        "designating them as <b>targets</b>.<br>" +
+        "Shortly after, the <span style=\"color:red;\">red</span> labels will dissapear and the dots will begin to move.<br>" +
+      "image", "test.png", false
+    ],
+    [
+      "Your task is to keep track of the <b>targets</b> as they move throughout the scene.<br>",
+      "movie", "test.mp4", false
+    ],
+    [
+      "At the end of each trial, <b>1</b> of the <b>8</b> dots will be highlighted in <span style=\"color:blue;\">blue</span>" +
+        ".<br> Your job is to judge whether that dot was one of the <b>targets</b>.",
+      "movie", "test.mp4", false
+    ],
+    [
+      "You will be able to record your response by clicking on one of the two check boxes shown below. <br>",
+        "<hr /><i>Note</i>: You will <b>NOT</b> be able to progress to the next trial until you have submitted a response.",
+      "text", "", true
+    ],
+    [
+      "You will be able to record your response by clicking on one of the two check boxes shown below. <br>" +
+        "<hr /><i>Note</i>: You will <b>NOT</b> be able to record your response until the video has <b>completed</b>",
+      "movie", "test.mp4", true
+    ],
     [
       "<b>Before we begin, follow the instructions below to setup your display.</b><br><hr />" +
         "<p>Please sit comfortably in front of you monitor and outstretch your arm holding a credit card (or a similary sized ID card). <br>" +
