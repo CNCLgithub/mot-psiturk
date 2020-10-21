946c1a73d626c4d0940c934ae008ee4bb555b9fd
eivinasbutkus <eivinas.butkus@yale.edu>
2020-10-21 00:24:30 -0400

    clickable trackers!

--- psiturk/static/js/dot_animation.js
+++ psiturk/static/js/dot_animation.js
@@ -7,7 +7,7 @@ class DotAnimation {
 
         this.k = this.positions.length;
         this.n_dots = this.positions[1].length;
-        this.n_dots = 2;
+        this.n_dots = 8;
 
         // collecting dots as JS objects
         this.dots = [];
@@ -21,29 +21,45 @@ class DotAnimation {
             }
 
             this.dots.push(dot);
+
+            anime.set(this.dots[i], {
+                translateX: this.positions[0][i][0],
+                translateY: -this.positions[0][i][1],
+            })
         }
 
     }
 
     play() {
-        const tl = anime.timeline({
-          loop: false,
-          autoplay: true,
-          duration: 500,
-          easing: 'linear',
-        });
-        
-        for (var t = 0; t < this.k; t++) {
-            for (var i = 0; i < this.n_dots; i++) {
-                tl.
-                add({
-                    targets: this.dots[i],
-                    easing: 'linear',
-                    loop: this.loop,
-                    translateX: this.positions[t][i][0],
-                    translateY: -this.positions[t][i][1],
-                }, t*this.duration)
-            }
+        for (var i = 0; i < this.n_dots; i++) {
+            anime({
+                targets: this.dots[i],
+                easing: 'linear',
+                loop: this.loop,
+                translateX: this.positions.map(p_t => ({value: p_t[i][0], duration: this.duration})),
+                translateY: this.positions.map(p_t => ({value: -p_t[i][1], duration: this.duration})),
+            })
         }
+
+
+        //const tl = anime.timeline({
+          //loop: false,
+          //autoplay: true,
+          //duration: 500,
+          //easing: 'linear',
+        //});
+        
+        //for (var t = 0; t < this.k; t++) {
+            //for (var i = 0; i < this.n_dots; i++) {
+                //tl.
+                //add({
+                    //targets: this.dots[i],
+                    //easing: 'linear',
+                    //loop: this.loop,
+                    //translateX: this.positions[t][i][0],
+                    //translateY: -this.positions[t][i][1],
+                //}, t*this.duration)
+            //}
+        //}
     }
 }
